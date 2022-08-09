e649b1aff136d7e7440869074b88524e804abeed
Alexandr Katyshev <katyshev1@gmail.com>
2022-08-09 19:22:10 +0300

    task-8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+const onFormSend = event => {
+    event.preventDefault();
+
+    const { elements: { email, password } } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("All of the form fields must be filled!");
+    }
+
+    const receivedData = {
+        email: email.value,
+        password: password.value,
+    }
+
+    console.log(receivedData);
+
+    event.currentTarget.reset();
+}
+
+form.addEventListener("submit", onFormSend);
